0b469cbf75d58726323546247fbc93ee363fb2cd
Austin Ziegler <austin@zieglers.ca>
2015-10-26 00:42:33 -0400

    Simplify color name definition tests

--- lib/color/rgb.rb
+++ lib/color/rgb.rb
@@ -676,18 +676,10 @@ end
 
 class << Color::RGB
   private
-  if RUBY_VERSION >= "1.9"
-    def __color_exists?(mod, name)
-      mod.const_defined?(name, false)
-    end
-  else
-    def __color_exists?(mod, name)
-      mod.const_defined?(name)
-    end
-  end
 
   def __named_color(mod, rgb, *names)
-    if names.any? { |n| __color_exists?(mod, n) }
+    used = names - mod.constants.map(&:to_sym)
+    if used.length < names.length
       raise ArgumentError, "#{names.join(', ')} already defined in #{mod}"
     end
 
