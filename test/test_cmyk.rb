4eb8039b2cb6d0ea683516201575ced8201dc3c1
Paul Gallagher <gallagher.paul@gmail.com>
2025-09-05 15:17:33 +0800

    refactor conversions tests and ensure full coverage
    Signed-off-by: Paul Gallagher <gallagher.paul@gmail.com>

--- test/test_cmyk.rb
+++ test/test_cmyk.rb
@@ -30,39 +30,6 @@ module TestColor
       assert_in_tolerance(40, @cmyk.black)
     end
 
-    def test_to_cmyk
-      assert(@cmyk.to_cmyk == @cmyk)
-    end
-
-    def test_to_grayscale
-      gs = @cmyk.to_grayscale
-      assert_kind_of(Color::Grayscale, gs)
-      assert_in_tolerance(0.4185, gs.g)
-      assert_kind_of(Color::Grayscale, @cmyk.to_grayscale)
-    end
-
-    # def test_to_hsl
-    #   hsl = @cmyk.to_hsl
-    #   assert_kind_of(Color::HSL, hsl)
-    #   assert_in_tolerance(0.48, hsl.l)
-    #   assert_in_tolerance(0.125, hsl.s)
-    #   assert_in_tolerance(0.08333, hsl.h)
-    # end
-
-    def test_to_rgb
-      rgb = @cmyk.to_rgb(rgb_method: :adobe)
-      assert_kind_of(Color::RGB, rgb)
-      assert_in_tolerance(0.5, rgb.r)
-      assert_in_tolerance(0.4, rgb.g)
-      assert_in_tolerance(0.3, rgb.b)
-
-      rgb = @cmyk.to_rgb
-      assert_kind_of(Color::RGB, rgb)
-      assert_in_tolerance(0.54, rgb.r)
-      assert_in_tolerance(0.48, rgb.g)
-      assert_in_tolerance(0.42, rgb.b)
-    end
-
     def test_inspect
       assert_equal("CMYK [10.00% 20.00% 30.00% 40.00%]", @cmyk.inspect)
     end
@@ -78,13 +45,75 @@ module TestColor
       assert_equal("device-cmyk(10.00% 20.00% 30.00% 40.00% / 0.50, rgb(0 0 100.00% / 0.30))", @cmyk.css(alpha: 0.5, fallback: {color: Color::RGB::Blue, alpha: 0.3}))
       assert_equal("device-cmyk(10.00% 20.00% 30.00% 40.00% / 0.50, rgb(54.00% 48.00% 42.00% / 0.30))", @cmyk.css(alpha: 0.5, fallback: {alpha: 0.3}))
     end
+  end
 
-    # def test_to_yiq
-    #   yiq = @cmyk.to_yiq
-    #   assert_kind_of(Color::YIQ, yiq)
-    #   assert_in_tolerance(0.4911, yiq.y)
-    #   assert_in_tolerance(0.05502, yiq.i)
-    #   assert_in_tolerance(0.0, yiq.q)
-    # end
+  class TestCMYKConversions < Minitest::Test
+    def setup
+      @cmyk = Color::CMYK.from_percentage(10, 20, 30, 40)
+    end
+
+    def test_to_cmyk
+      assert_equal(@cmyk, @cmyk.to_cmyk)
+      assert_kind_of(Color::CMYK, @cmyk.to_cmyk)
+    end
+
+    def test_to_grayscale
+      gs = @cmyk.to_grayscale
+      assert_kind_of(Color::Grayscale, gs)
+      assert_in_tolerance(0.4185, gs.g)
+    end
+
+    def test_to_hsl
+      hsl = @cmyk.to_hsl
+      assert_kind_of(Color::HSL, hsl)
+      assert_in_tolerance(0.48, hsl.l)
+      assert_in_tolerance(0.125, hsl.s)
+      assert_in_tolerance(0.08333, hsl.h)
+    end
+
+    def test_to_lab
+      lab = @cmyk.to_lab
+      assert_kind_of(Color::CIELAB, lab)
+      assert_in_tolerance(52.35269, lab.l)
+      assert_in_tolerance(3.268274, lab.a)
+      assert_in_tolerance(10.52531, lab.b)
+      assert_equal(Color::CIELAB.from_values(0, 0, 0), Color::CMYK.from_percentage(0, 0, 0, 100).to_lab)
+    end
+
+    def test_to_rgb
+      rgb_adobe = @cmyk.to_rgb(rgb_method: :adobe)
+      assert_kind_of(Color::RGB, rgb_adobe)
+      assert_in_tolerance(0.5, rgb_adobe.r)
+      assert_in_tolerance(0.4, rgb_adobe.g)
+      assert_in_tolerance(0.3, rgb_adobe.b)
+
+      rgb = @cmyk.to_rgb
+      assert_kind_of(Color::RGB, rgb)
+      assert_in_tolerance(0.54, rgb.r)
+      assert_in_tolerance(0.48, rgb.g)
+      assert_in_tolerance(0.42, rgb.b)
+    end
+
+    def test_to_xyz
+      xyz = @cmyk.to_xyz
+      assert_kind_of(Color::XYZ, xyz)
+      assert_in_tolerance(0.200995, xyz.x)
+      assert_in_tolerance(0.204594, xyz.y)
+      assert_in_tolerance(0.168249, xyz.z)
+      assert_equal(Color::XYZ.from_values(0, 0, 0), Color::CMYK.from_percentage(0, 0, 0, 100).to_xyz)
+    end
+
+    def test_to_yiq
+      yiq = @cmyk.to_yiq
+      assert_kind_of(Color::YIQ, yiq)
+      assert_in_tolerance(0.4911, yiq.y)
+      assert_in_tolerance(0.05502, yiq.i)
+      assert_in_tolerance(0.0, yiq.q)
+      assert_equal(Color::YIQ.from_values(0, 0, 0), Color::CMYK.from_percentage(0, 0, 0, 100).to_yiq)
+    end
+
+    def test_to_internal
+      assert_equal([0.0, 0.0, 0.0, 1.0], Color::CMYK.from_percentage(0, 0, 0, 100).to_internal)
+    end
   end
 end
